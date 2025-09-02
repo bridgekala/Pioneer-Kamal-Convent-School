2f4b9b123419b45bd5e78592c1369dd10f11d81c
thenikhilsingh <149455116+thenikhilsingh@users.noreply.github.com>
2025-09-02 18:00:08 +0530

    chsnge

--- resources/views/Frontend/Partials/style.blade.php
+++ resources/views/Frontend/Partials/style.blade.php
@@ -1,7 +1,7 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Pioneer Kamal Convent</title>
-<link rel="shortcut icon" type="image/x-icon" href="{{ asset('assets/images/logo/logo1.png') }}">
+<link rel="shortcut icon" type="image/x-icon" href="{{ asset('assets/images/logo/logowbg.png') }}">
 <!-- animate css -->
 <link rel="stylesheet" href="{{ asset('assets/css/plugins/animate.min.css') }}">
 <!-- fontawesome 6.4.2 -->
