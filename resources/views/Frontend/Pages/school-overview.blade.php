ec9af8d40debf7f79df74ee3f335bfa5bb360a88
thenikhilsingh <149455116+thenikhilsingh@users.noreply.github.com>
2025-09-02 17:30:08 +0530

    done

--- resources/views/Frontend/Pages/school-overview.blade.php
+++ resources/views/Frontend/Pages/school-overview.blade.php
@@ -15,23 +15,18 @@
                 <div class="col-lg-6 ">
                     <img style="height: 400px; width:600px" src="/assets/images/school.jpg" alt="">
                 </div>
-                <div  class="col-lg-6">
+                <div class="col-lg-6">
                     <h4>About Our School</h4>
-<<<<<<< HEAD
-                    <p class="desc"> Pioneer Kamal Convent School, located in Lok Nayak Puram, Bakkarwala, New Delhi, is
-=======
-                    <p class="desc">Pioneer Kamal Convent School, located in Lok Nayak Puram, Bakkarwala, New Delhi, is
->>>>>>> 48b9f4350d72c3e5c0c5973f9eeeeb9affb639d7
+                    <p class="desc">Pioneer Kamal Convent School, located in A Block, Pioneer Road Vikas Nagar (Vikaspuri
+                        Extension) New Delhi-110059 Delhi, India 110059
+                        Delhi, is
                         a distinguished institution committed to academic excellence and holistic development. Nestled
                         within a lush green 2-acre campus, the school provides a nurturing environment enriched with
                         state-of-the-art infrastructure, comprehensive sports facilities, and a wide array of co-curricular
                         activities.
 
-<<<<<<< HEAD
-                        Affiliated with CBSE up to Class XII and offering all academic streams,  Pioneer Kamal believes in
-=======
-                        Affiliated with CBSE up to Class XII and offering all academic streams, Pioneer Kamal Convent believes in
->>>>>>> 48b9f4350d72c3e5c0c5973f9eeeeb9affb639d7
+                        Affiliated with CBSE up to Class XII and offering all academic streams, Pioneer Kamal Convent
+                        believes in
                         developing strong mind-body-soul coordination in students through a thoughtfully designed
                         curriculum. From robotics and language labs to music, arts, and sports, every element of school life
                         fosters creativity, critical thinking, and personal growth. With a dedicated faculty and a
@@ -40,11 +35,8 @@
                     </p>
                 </div>
                 <h4>Recognition and Affiliation</h4>
-<<<<<<< HEAD
-                <p> Pioneer Kamal School is affiliated to the Central Board of Secondary Education (CBSE), Delhi, with
-=======
-                <p>Pioneer Kamal Convent School is affiliated to the Central Board of Secondary Education (CBSE), Delhi, with
->>>>>>> 48b9f4350d72c3e5c0c5973f9eeeeb9affb639d7
+                <p>Pioneer Kamal Convent School is affiliated to the Central Board of Secondary Education (CBSE), Delhi,
+                    with
                     Affiliation Number 2730606 and School Code 85345. The school is recognized under the RTE Act, 2009, and
                     adheres to all mandatory norms, including possession of valid Fire Safety and Building Safety
                     Certificates, DEO Certification for Affiliation, and Water, Health, and Sanitation Certificates. Our
@@ -52,7 +44,8 @@
                     performance, achieving a 100% pass rate in Class X and over 90% success in Class XII examinations in
                     recent academic sessions.</p>
                 <h4>Aims and Objectives</h4>
-                <p style="margin-bottom: 100px">At Pioneer Kamal Convent, our mission is to educate young leaders equipped for global citizenship. We strive to
+                <p style="margin-bottom: 100px">At Pioneer Kamal Convent, our mission is to educate young leaders equipped
+                    for global citizenship. We strive to
                     deliver holistic education that seamlessly integrates academics, co-curricular activities, and essential
                     life skills. By connecting students with nature and real-world challenges, we aim to inspire creativity,
                     innovation, and resilience. Deeply rooted in Indian cultural values such as honesty, discipline,
